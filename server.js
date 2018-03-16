23ae526cb91223882647a74b19e4e0117213fc23
desertrockclimber <{desertrockclimber}@users.noreply.github.com>
2018-03-15 19:36:02 -0600

    push project

--- server.js
+++ server.js
@@ -6,20 +6,23 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 4000;
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // var jsonParser = bodyParser.json()
-
-app.use(bodyParser.urlencoded({extended: true}));
-
 var app = express();
-app.use(bodyParser.json({type: 'application/++json'}))
+
+app.use(express.static("./app/public"))
+
+app.use(bodyParser.json({type: 'application/*+json'}))
 
 app.use(bodyParser.raw({type: 'application/vnd.custom-type'}))
 
 app.use(bodyParser.text({type: 'text/html'}))
 
-require("./routing/apiRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
 
-require("./routing/htmlRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 
 app.listen(PORT, function() {
